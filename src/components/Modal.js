be7f1b1ea8528b8dc0f660b055f002619237bef4
Akashkp-creator <akp979760@gmail.com>
2025-08-25 08:45:51 +0530

    Created ModalSlice

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,6 +3,14 @@ const Modal = () => {
     <aside className="modal-container">
       <div className="modal">
         <h4>remove all items from your shopping cart?</h4>
+        <div className="btn-container">
+          <button type="button" className="btn confirm-btn">
+            confirm
+          </button>
+          <button type="button" className="btn clear-btn">
+            clear
+          </button>
+        </div>
       </div>
     </aside>
   );
--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/cartSlice"; // v have imported the reducer fun. from the cartSlice
+import modalReducer from "./features/modal/modalSlice";
 export const store = configureStore({
   reducer: {
-    // "cart" is a key name u can give any thing as u wish.
+    // "cart" is a key name u can give any thing as u wish. cartReducer is reducer fun. form cartSlice
     cart: cartReducer,
+    // "modal" is a key name . modalReducer is reducer fun. from modalSlice
+    modal: modalReducer,
   },
 });
