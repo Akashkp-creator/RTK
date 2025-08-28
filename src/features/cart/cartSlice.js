ba878876a91ce32813927cae5b0c7a5f15fb43a2
Akashkp-creator <akp979760@gmail.com>
2025-08-28 13:26:37 +0530

    AsyncThunk ,  Builder call back fun. in cartSlice very imp for async thing

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,11 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { openModal } from "../modal/modalSlice";
+
+const url = "https://www.course-api.com/react-useReducer-cart-project";
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 1,
   total: 0,
   isLoading: true,
 };
+
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems", //"cart/getCartItems" is action
+  async (name, thunkAPI) => {
+    // return fetch(url)
+    //   .then((resp) => resp.json())
+    //   .catch((err) => console.log(err));
+    try {
+      // console.log(name); //it is sent from the App.js file. O/P is "random".
+      // console.log(thunkAPI); //v have "dispatch" to access the dispatch & "getStart" to access the state.
+      // console.log(thunkAPI.getState()); //to get the state of all the features (from modal,cart)
+      // thunkAPI.dispatch(openModal()); //v can also access the feature of "openModel"
+      const resp = await axios(url);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("something went wrong");
+    }
+  }
+);
 const cartSlice = createSlice({
   name: "cart", //any thing u can give but give it as the feature related name (suggestion)
   initialState,
@@ -45,6 +67,22 @@ const cartSlice = createSlice({
       state.amount = amount;
       state.total = total;
     },
+  },
+  // builder callback function
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        // console.log(action);
+        state.isLoading = false;
+        state.cartItems = action.payload;
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        console.log(action);
+        state.isLoading = false;
+      });
   },
 });
 
